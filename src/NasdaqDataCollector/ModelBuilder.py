f8d6d584b4b9ef470dbe14018969ea7c75d35d3c
Shashank Mandil <mandil.shashank@gmail.com>
2017-09-22 14:06:56 +0000

    Building a new model based on multiple weekly data

--- src/NasdaqDataCollector/ModelBuilder.py
+++ src/NasdaqDataCollector/ModelBuilder.py
@@ -1,8 +1,10 @@
-from src.NasdaqDataCollector.StockPredictor import StockPredictor
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+from NasdaqDataCollector.StockPredictor import StockPredictor
 
 if __name__ == "__main__":
     StockPredictor.build_models()
-
 
 
 
